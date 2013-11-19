6eb31d8a351e713b273ed8e7803ea4980d262804
Michael Jones <m.pricejones@gmail.com>
2013-11-20 12:13:34 +1300

    Tags, virtualenvs, env var, center selection
    All mixed together as I forget to commit in my rambling explorations of
    haskell.
    
    This introduces an optional "tags" entry into the locations listed in
    the yaml file. This is for extra meta-data which we use to highlight the
    Python virtual env for each location if there.
    
    We then change the output to check for the virtualenv and write out the
    activate line to the output file, also check for a current virtualenv and
    write out a decactivate line, as well as the "cd" command which is what
    jump is meant to do.
    
    Could be a lot nicer I imagine but that will come with time.
    
    We also schedule a change to center the selection on the list at start
    up. This cannot be done (from trial and error) before the UI loop as
    kicked in so we use "schudule" to add it to the loop to be run once
    everything is up and running.
    
    And fixed various ghc compiler warnings
    
    - No type signatures
    - Import semantics (Extra ()s)
    - Ignoring statement result in do section (_ <- ...)
    - Unused record names (getDirectory getTags)
    - Variable shadowing (border)
    - Missing patterns (Nothing)

--- src/Main.hs
+++ src/Main.hs
@@ -4,33 +4,41 @@ import           Graphics.Vty.Widgets.All
 import           Graphics.Vty.Attributes ( def_attr )
 import           Graphics.Vty.LLInput    ( Key( KASCII) )
 
-import           System.IO   ( writeFile )
+import           System.Environment ( lookupEnv )
+import           System.IO()
 import           System.Exit ( exitSuccess )
 
 import           Control.Monad
 import           Control.Applicative
 
-import           Data.Maybe ( isJust )
+import           Data.Maybe ( isJust, fromJust )
 
+import qualified Data.Map.Strict as M
+import qualified Data.List as L()
 import qualified Data.Text as T
 import qualified Data.Yaml as Y
-import           Data.Yaml ( (.:) )
-import qualified Data.Vector as V
+import           Data.Yaml ( (.:), (.:?) )
+import qualified Data.Vector as V()
+import qualified Data.IORef as R
 
 main :: IO ()
 main = do
 
-    results <- Y.decodeFile "jumprc" :: IO ( Maybe [Location] )
+    jumprc <- lookupEnv "JUMP_CONFIG"
+
+    results <- Y.decodeFile ( fromJust jumprc ) :: IO ( Maybe [Location] )
 
     when (isJust results) $ do
 
         let pairs = case results of (Just locations) -> locations
+                                    Nothing -> []
             listLength = length pairs
             entryWidth = maximum $ map (length . getName) pairs
             padding = 4
-            border = 2
-            borderedWidth = entryWidth + border + padding
-            borderedHeight = listLength + border
+            borderThickness = 2
+            borderedWidth = entryWidth + borderThickness + padding
+            borderedHeight = listLength + borderThickness
+            middle = truncate $ ( toRational listLength ) / 2.0
 
         -- Create new list
         list <- newList def_attr
@@ -44,7 +52,7 @@ main = do
         ui <- centered fixed
 
         fg <- newFocusGroup
-        addToFocusGroup fg list
+        _ <- addToFocusGroup fg list
 
         c <- newCollection
         _ <- addToCollection c ui fg
@@ -53,44 +61,78 @@ main = do
         fg `onKeyPressed` exit
 
         -- List event handlers
-        list `onItemActivated` writeResult
+        list `onItemActivated` handleSelection
         list `onKeyPressed` navigate
+
+        -- We want to highlight the middle item of the list to begin with but
+        -- we can't do that untill everything has been set up so we schedule
+        -- the task so that it happens in the event loop when everything is up
+        -- and running
+        schedule $ scrollBy list middle
 
         runUi c defaultContext
 
 -- Callback for exiting via 'q'
+exit :: a -> Key -> b -> IO Bool
 exit _ key _ | key == KASCII 'q' = do { shutdownUi; exitSuccess }
              | otherwise         = return False
 
 -- Callback for list nagivation
+navigate :: Widget (List a b) -> Key -> c -> IO Bool
 navigate list key _ | key == KASCII 'j' = handle $ scrollDown list
                     | key == KASCII 'k' = handle $ scrollUp list
                     | otherwise         = return False
     where 
-        handle x = do { x; return True }
+        handle x = do { _ <- x; return True }
 
 -- Callback for list item selection
-writeResult :: ActivateItemEvent String b -> IO ()
-writeResult event = do
+handleSelection :: ActivateItemEvent (Directory, Maybe Tags) b -> IO ()
+handleSelection event = do
     let ActivateItemEvent _ a _ = event
+        directory = fst a
+        tags = snd a
 
-    writeFile "/tmp/jump-hs.sh" a
+    iolist <- R.newIORef ["cd " ++ directory ++ ";\n"]
+
+    currentVirtualEnv <- lookupEnv "VIRTUAL_ENV"
+    R.modifyIORef iolist $ lastVirtualEnvAction (isJust currentVirtualEnv)
+    R.modifyIORef iolist $ newVirtualenvAction tags
+
+    output <- R.readIORef iolist
+    writeFile "/tmp/jump-hs.sh" $ concat output
     shutdownUi
 
+
+-- If there is a "virtualenv" tag then return a function that appends the
+-- correct commands to the file to be written
+newVirtualenvAction :: Maybe Tags -> ([String] -> [String])
+newVirtualenvAction (Just a) = case M.lookup "virtualenv" a of
+    (Just value) -> let activate = "source " ++ value ++ "/bin/activate;\n"
+                    in  (++ [activate])
+    Nothing      -> id
+newVirtualenvAction Nothing  = id
+
+-- Returns action to perform if we're in a virtualenv already
+lastVirtualEnvAction :: Bool -> ([String] -> [String])
+lastVirtualEnvAction True  = (++ ["deactivate;\n"])
+lastVirtualEnvAction False = id
+
 -- Add processed yaml data to the list
-addPairsToList :: Widget (List Directory FormattedText) -> Location -> IO ()
-addPairsToList list (Location name dir) = addToList list dir =<< ( plainText $ T.pack name )
+addPairsToList :: Widget (List (Directory, Maybe Tags) FormattedText) -> Location -> IO ()
+addPairsToList list (Location name dir tags) = addToList list (dir, tags) =<< ( plainText $ T.pack name )
 
 type Name = String
 type Directory = String
+type Tags = M.Map String String
 
-data Location = Location { getName :: Name, getDirectory :: Directory } deriving Show
+data Location = Location { getName :: Name, _getDirectory :: Directory, _getTags :: Maybe Tags } deriving Show
 
 -- Specify FromJSON instance for Location to tie into Yaml deserialisation
 instance Y.FromJSON Location where
    parseJSON (Y.Object v) = Location <$>
-                            v .: T.pack "name" <*>
-                            v .: T.pack "directory"
+                            v .:  T.pack "name" <*>
+                            v .:  T.pack "directory" <*>
+                            v .:? T.pack "tags"
 
    parseJSON _            = mzero
 
